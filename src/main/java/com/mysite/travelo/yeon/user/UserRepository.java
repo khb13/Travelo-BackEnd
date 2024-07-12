4e6f9fddcacd9a2886f6d2f5e156dc851a984f31
khb13 <120230839+khb13@users.noreply.github.com>
2024-07-12 10:52:58 +0900

    [Remove] 작업 패키지 외 삭제

--- src/main/java/com/mysite/travelo/yeon/user/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mysite.travelo.yeon.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    User findById(int UserSeq);
-}
--- src/main/java/com/mysite/travelo/yeon/user/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.mysite.travelo.yeon.user;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserService {
-
-}
